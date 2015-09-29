cca966b2b88b534f58c77edc0c75be6b972a1f5e
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-29 12:17:49 +0530

    first commit

---
+++ WebApplication_SubModule/WebApplication_GetDate/DateFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_GetDate
+{
+    public class DateFile
+    {
+
+        public string get_Date()
+        {
+            string currDate = string.Empty;
+            currDate = DateTime.Now.ToString();
+            return currDate;
+
+        }
+
+    }
+}
